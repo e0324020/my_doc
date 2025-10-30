f06f2c1fd676ef6fd1a04b3047e44d8bef0f0576
e0324020 <e0324020@sriher.edu.in>
2025-10-30 13:36:59 +0530

    Create scatterplot.py

---
+++ scatterplot.py
@@ -0,0 +1,7 @@
+hours_studied = [15, 20, 12, 8, 18, 10, 22, 14, 9, 17]
+
+plt.scatter(hours_studied, math_marks, color='red')
+plt.title("Relationship Between Study Hours and Math Marks")
+plt.xlabel("Hours Studied Per Week")
+plt.ylabel("Math Marks")
+plt.show()
